52b05a04b807fd7f9b32bedb0a824fc8176b6cba
Andrea Ravalli <anravalli@gmail.com>
2022-05-17 02:57:15 +0200

    Monster Chase #33 - Game Pages Changes: - src/monsterchase.cpp: creating and maniging option page - src/options_panel/optionpage_controller.cpp/h: option page definition and base controll logic - src/options_panel/optionpage_view.cpp/h: option page initial view - src/src.pro: added optino page controller and view to the project

---
+++ src/options_panel/optionpage_controller.cpp
@@ -0,0 +1,162 @@
+/*
+ * optionpage_controller.cpp
+ *
+ *  Created on: 24 mar 2022
+ *      Author: andrea
+ */
+
+#include "optionpage_controller.h"
+#include "optionpage_view.h"
+
+OptionPageController::OptionPageController(UiPageController *parent):
+	UiPageController(parent)
+{
+
+	is_saved = true;
+	initPageView<OptionPageView>();
+
+	vector<QString> model = {"Screen Resolution","Fullscreen","Sounds", "APPLY", "EXIT"};
+	vector<function<void()>> actions;
+
+	actions.push_back([this]{
+		this->change_screen_resolution();
+	});
+
+	actions.push_back([this]{
+		this->set_fullscreen();
+	});
+
+	actions.push_back([this]{
+		this->set_sounds();
+	});
+
+	actions.push_back([this]{
+		this->apply_settings();
+	});
+
+	actions.push_back([this]{
+		this->open_confirm_popup();
+	});
+
+	options_menu = new UiPageMenu(actions, model);
+
+	vector<QString> popup_model = {"apply", "discard"};
+	vector<function<void()>> popup_actions;
+	popup_actions.push_back([this]{
+		this->apply_settings();
+		this->close_confirm_popup();
+		this->exit();
+	});
+	popup_actions.push_back([this]{
+		this->close_confirm_popup();
+		this->exit();
+	});
+	auto *popup_view = new UiPagePopupWidget_qt(QString("Do you want to apply your changes before exit?"),
+			new UiPageMenuWidget_qt(&popup_model));
+
+	confirm_exit_menu = new UiPageMenu(popup_actions, popup_view, 1);
+	confirm_exit_menu->hide();
+	confirm_exit_menu->addToPage(page_view);
+
+	active_menu = options_menu;
+	active_menu->addToPage(page_view);
+}
+
+OptionPageController::~OptionPageController()
+{
+	// TODO Auto-generated destructor stub
+}
+
+void OptionPageController::show(){
+	//load saved config
+	//update config model
+    UiPageController::show();
+}
+
+void OptionPageController::exit()
+{
+    qDebug("OptionPageController::exit()");
+    qDebug("is_saved %d", is_saved);
+    UiPageController::exit();
+//    if(is_saved)
+//    else{
+//    	//confirm unsaved options;
+//    	is_saved = true;
+//    }
+}
+
+bool OptionPageController::handleKey(int key, bool released)
+{
+
+    bool ret = false;
+    switch(key){
+    case Qt::Key_Up:
+    case Qt::Key_W:
+    case Qt::Key_S:
+    case Qt::Key_Down:
+    case Qt::Key_Enter:
+    case Qt::Key_Return:
+    	ret = active_menu->handleKey(key, released);
+    	break;
+    case Qt::Key_Exit:
+    case Qt::Key_Escape:
+    	if (released)
+    	{
+    		if(active_menu == confirm_exit_menu)
+    		{
+    			close_confirm_popup();
+    		}
+    		else if (!is_saved)
+    		{
+    			open_confirm_popup();
+    		}
+    		else
+    			exit();
+    	}
+        ret = true;
+        break;
+    default:
+        break;
+    }
+    return ret;
+
+}
+
+void OptionPageController::open_confirm_popup()
+{
+	if (!is_saved)
+	{
+		this->active_menu->deactivate();
+		this->active_menu = this->confirm_exit_menu;
+		this->active_menu->show();
+	}
+	else
+		this->exit();
+}
+
+void OptionPageController::close_confirm_popup()
+{
+	this->active_menu->hide();
+	this->active_menu = this->options_menu;
+	this->active_menu->activate();
+}
+
+void OptionPageController::change_screen_resolution()
+{
+	is_saved = false;
+}
+
+void OptionPageController::set_fullscreen()
+{
+	is_saved = false;
+}
+
+void OptionPageController::set_sounds()
+{
+	is_saved = false;
+}
+
+void OptionPageController::apply_settings()
+{
+	is_saved = true;
+}
